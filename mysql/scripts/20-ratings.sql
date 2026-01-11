a1b73b75392c73cf514292ebec3bdf368e1dcbb5
devops4sure <devops4sure@gmail.com>
2026-01-11 10:04:26 -0800

    update

--- mysql/scripts/20-ratings.sql
+++ mysql/scripts/20-ratings.sql
@@ -1,17 +1,16 @@
+-- Create user and grant privileges
 CREATE USER 'ratings'@'%' IDENTIFIED BY 'iloveit';
 GRANT ALL PRIVILEGES ON ratings.* TO 'ratings'@'%';
 FLUSH PRIVILEGES;
 
+-- Use the database
 USE ratings;
 
+-- Create table
 CREATE TABLE ratings (
-    sku varchar(80) NOT NULL,
-    avg_rating DECIMAL(3, 2) NOT NULL,
+    sku VARCHAR(80) NOT NULL,
+    avg_rating DECIMAL(3,2) NOT NULL,
     rating_count INT NOT NULL,
     PRIMARY KEY (sku)
 ) ENGINE=InnoDB;
-
-
-GRANT ALL ON ratings.* TO 'ratings'@'%'
-IDENTIFIED BY 'iloveit';
 
