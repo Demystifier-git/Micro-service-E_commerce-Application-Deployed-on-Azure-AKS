c9f1296f5a52ddac37408675213f5f3987f53b89
devops4sure <devops4sure@gmail.com>
2026-01-11 20:05:20 -0800

    update

--- mysql/scripts/20-ratings.sql
+++ mysql/scripts/20-ratings.sql
@@ -1,16 +1,22 @@
--- Create user and grant privileges
-CREATE USER 'ratings'@'%' IDENTIFIED BY 'iloveit';
+-- Create user (without specifying a password)
+CREATE USER 'ratings'@'%';
+
+-- Grant privileges on the ratings database
 GRANT ALL PRIVILEGES ON ratings.* TO 'ratings'@'%';
 FLUSH PRIVILEGES;
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS ratings;
 
 -- Use the database
 USE ratings;
 
--- Create table
+-- Create the table
 CREATE TABLE ratings (
     sku VARCHAR(80) NOT NULL,
     avg_rating DECIMAL(3,2) NOT NULL,
     rating_count INT NOT NULL,
     PRIMARY KEY (sku)
 ) ENGINE=InnoDB;
+
 
